64cb2e9f5f7c1246db638bbc3554cf25857bda8f
zkl <zkl@email.com>
2022-09-12 11:20:48 +0800

    dev分支

--- src/main/java/com/xxxx/Test02.java
+++ src/main/java/com/xxxx/Test02.java
@@ -3,6 +3,6 @@ package com.xxxx;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println("test02....");
-        System.out.println("主1分支 -> test02....");
+        System.out.println("dev分支 -> test02....");
     }
 }
